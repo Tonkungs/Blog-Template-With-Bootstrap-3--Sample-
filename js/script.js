988d286ddf199c54af1248d79310cd859f62e402
Tonkung <ton_naruto_v.2@hotmail.com>
2016-05-15 18:22:59 +0700

    Update 0.2
    Blog Template  (Sample)

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$("#clickme").click(function(){
+	$("#book").hide("slow",function(){
+		alert("Animation Complete");
+	});
+});
+$(".Post").click(function(){
+	$("#post").hide("slow",function(){
+		alert("Animation Complete");
+	});
+});
